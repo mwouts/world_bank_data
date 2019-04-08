d59b0fb6a7fddc3faacd9cf306ef2598277130fd
Marc Wouts <marc.wouts@gmail.com>
2019-04-09 01:37:23 +0200

    Python 3.4-5 may change column order

--- tests/test_others.py
+++ tests/test_others.py
@@ -5,21 +5,21 @@ from .tools import assert_numeric_or_string
 def test_lending_types():
     df = get_lendingtypes()
     assert df.index.names == ['id']
-    assert df.columns.to_list() == ['iso2code', 'value']
+    assert set(df.columns) == set(['iso2code', 'value'])
     assert_numeric_or_string(df)
 
 
 def test_income_levels():
     df = get_incomelevels()
     assert df.index.names == ['id']
-    assert df.columns.to_list() == ['iso2code', 'value']
+    assert set(df.columns) == set(['iso2code', 'value'])
     assert_numeric_or_string(df)
 
 
 def test_topics():
     df = get_topics()
     assert df.index.names == ['id']
-    assert df.columns.to_list() == ['value', 'sourceNote']
+    assert set(df.columns) == set(['value', 'sourceNote'])
     assert_numeric_or_string(df)
 
 
